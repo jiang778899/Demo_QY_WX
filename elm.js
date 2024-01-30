79504c6456b155ee7b580dee6270d8c4a2d5cae1
jiang778899 <82449107+jiang778899@users.noreply.github.com>
2024-01-30 21:15:34 +0800

    6

---
+++ elm.js
@@ -0,0 +1,28 @@
+body = {
+    "code": 200,
+    "data": {
+        "favorableTotalPrice": null,
+        "allBenefitPrice": null,
+        "payingJournalIds": null,
+        "phone": "",
+        "forbiddenReason": null,
+        "baseInfoBeforePlace": null,
+        "riskControlExpress": null,
+        "isPermitted": true,
+        "total": null,
+        "perfectPlace": 1,
+        "isElemeUser": 0,
+        "isPaying": null,
+        "depositKnightAlert": 0,
+        "payRightPrice": null,
+        "needPayOrderDate": null,
+        "needPayOrders": null,
+        "originalTotalPrice": null,
+        "totalPrice": null,
+        "violate": false
+    },
+    "success": true,
+    "msg": "请求成功"
+};
+
+$done({body: JSON.stringify(body)}); //
