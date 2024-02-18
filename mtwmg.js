c61141a43f1f5b8ac101e0feae334181bf4e0181
jiang778899 <82449107+jiang778899@users.noreply.github.com>
2024-02-18 14:59:20 +0800

    Update mtwmg.js

--- mtwmg.js
+++ mtwmg.js
@@ -1,3 +1,25 @@
-body = $response.body.replace(/\"chargeMT":1/g, '\"chargeMT":0').replace(/\"charge":1/g, '\"charge":0');
+/*************************************
+
+项目功能：美团外卖柜解锁
+下载地址：vx:xiaozhuzi96
+脚本作者：竹子
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+QX
+[rewrite_local]
+^https:\/\/deliverycommonapi.peisong.meituan.com\/mp\/cabinet\/pickup\/cabinet\/getCabinetInfo url script-response-body mtwmg.js
+Shadowrocket
+美团外卖柜开柜 = type=http-response,pattern= ^https:\/\/deliverycommonapi.peisong.meituan.com\/mp\/cabinet\/pickup\/cabinet\/getCabinetInfo,requires-body=1,max-size=0,script-path=mtwmg.js
+[mitm] 
+hostname = deliverycommonapi.peisong.meituan.com
+
+*************************************/
+
+
+var body = $response.body;
+
+body = body.replace(/\"charge":1/g, '\"charge":0');
+body = body.replace(/\"chargeMT":1/g, '\"chargeMT":0');
 
 $done({body});
